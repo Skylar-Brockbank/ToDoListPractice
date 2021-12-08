a486abd15101673873a185ce7b870a3096b53493
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-12-08 09:46:49 -0800

    add return tests for static list
    Co-Authored-by: Albert Lee <leesga8@gmail.com>

--- ToDoList.Tests/ModelTests/ToDoLogicTests.cs
+++ ToDoList.Tests/ModelTests/ToDoLogicTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
     [TestMethod]
     public void ItemConstructor_DoesMakesItemsFrumClass_Thing()
     {
@@ -30,5 +36,28 @@ namespace ToDoList.Tests
       newItem.Description = updatedQuest;
       Assert.AreEqual(updatedQuest, newItem.Description);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item>{};
+      CollectionAssert.AreEqual(newList, Item.GetAll());
+    }
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string d1 = "Storm the Castle";
+      string d2 = "Save your father";
+      string d3 = "???";
+      string d4 = "profit?";
+      Item ni1 = new Item(d1);
+      Item ni2 = new Item(d2);
+      Item ni3 = new Item(d3);
+      Item ni4 = new Item(d4);
+      List<Item> newList = new List<Item> { ni1,ni2,ni3,ni4 };
+
+      CollectionAssert.AreEqual(newList, Item.GetAll());
+    }
+
   }
 }
--- ToDoList/Models/ToDoLogic.cs
+++ ToDoList/Models/ToDoLogic.cs
@@ -1,10 +1,22 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set; }
+    public static List<Item> _itemList = new List<Item>{};
     public Item(string description){
       Description = description;
+      _itemList.Add(this);
+    }
+    public static List<Item> GetAll()
+    {
+      return _itemList;
+    }
+    public static void ClearAll()
+    {
+      _itemList.Clear();
     }
   }
 }
